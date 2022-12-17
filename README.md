d1434dbc41f92ba23efcaceb5df9e9f95bfa32cf
Alison Schofield <alison.schofield@intel.com>
2022-12-16 19:09:06 -0800

    cxl/addrtest: Rework of test case generation & fixed lower bits
    See the README.md for new usage.
    
    There is still a sample output file, but no longer json format.
    No more shell program. Execute C program with a dpa_offset.
    Added all permutations of EIG EIW POS as test cases.
    
    Fixed lower bits where wasn't grabbing from dpa_offset to apply to
    hpa_offset.
    
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>

--- README.md
+++ README.md
@@ -6,17 +6,22 @@ cxl/addrtest: Out of tree test for CXL address translations
 Intends to mimic the address calculations found in the CXL kernel
 driver. Known differences, noted in the C file, relate to the
 creation of masks.
-The initial test cases include:
-- The 2 examples from the CXL Spec Decode Logic section.
-- The 1 example from the CXL Driver Writers Guide translation section
-- The 33 examples from the CXL test suite execution.
 
-Add new 'test cases' to addrx_data.json:
-Test cases look like this:
-{ "dpa": "0x0400", "eig": "2", "eiw": "3", "pos": "5", "hpa": "0x3400" }
+Usage: ./addrx dpa_offset
 
-DPA & HPA fields are OFFSETS.
+Example: $ ./addrx 0x40000008 > your_output_file
 
-./addrs.sh calls C program: "addrx" with each test case.
+A results file (addrx_sample.out) using dpa 0x40000008 is included in the repo.
 
-C program "addrx" verifies DPA->HPA and HPA->DPA translations.
+All permutations of EIG EIW POS are applied to the dpa_offset to make an hpa_offset,
+
+for each EIG
+	for each EIW
+		for each POS
+			Translate dpa_offset to hpa_offset & check
+
+$ grep Pass your_output_file | wc -l  
+357
+
+$ grep FAIL your_ouput_file
+0  
--- addrx.c
+++ addrx.c
@@ -12,13 +12,13 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 
 /*
  * "MY_" macros are trying (poorly?) to mimic kernel macros.
@@ -31,23 +31,13 @@
 	(((~(0)) - ((1) << (l)) + 1) & \
 	 (~(0) >> (MY_BITS_PER_LONG_LONG - 1 - (h))))
 
-
-/*
- * Intent is to be as close to possible as the kernel calculation.
- * At the moment, this differs due to the masking functions.
- */
-
-int main(int argc, char *argv[])
+unsigned long long to_dpa(unsigned long long hpa_offset,
+			  uint16_t eig, uint8_t eiw)
 {
-	unsigned long long dpa_offset = strtoull(argv[1], NULL, 0);
-	unsigned long long hpa_offset = strtoull(argv[2], NULL, 0);
 	unsigned long long mask_upper, mask_lower, dpa_upper_mask;
 	unsigned long long bits_upper, bits_lower;
-	unsigned long long test_offset;
-	int eig = atoi(argv[3]);
-	int eiw = atoi(argv[4]);
-	int pos = atoi(argv[5]);
-
+	unsigned long long dpa_offset;
+	
 	/*
 	 * Translate HPA to DPA
 	 *
@@ -58,44 +48,100 @@ int main(int argc, char *argv[])
 	 */
 	if (eiw < 8) {
 		mask_upper = MY_GENMASK_ULL(51, eig + 8 + eiw);
-		test_offset = (hpa_offset & mask_upper) >> eiw;
+		dpa_offset = (hpa_offset & mask_upper) >> eiw;
 	} else {
 		mask_upper = MY_GENMASK_ULL(51, eig + eiw);
 		bits_upper = (hpa_offset & mask_upper) >> eig + eiw;
 		bits_upper = bits_upper / 3;
-		test_offset = bits_upper << eig + 8;
+		dpa_offset = bits_upper << eig + 8;
 	}
 
 	/* Lower bits don't change */
 	mask_lower = (1 << (eig + 8)) -1;
 	bits_lower = hpa_offset & mask_lower;
-	test_offset += bits_lower;
+	dpa_offset += bits_lower;
 
-	if (test_offset != dpa_offset)
-		exit(EXIT_FAILURE);
+	return dpa_offset;
+}
 
+void to_hpa(unsigned long long dpa_offset, uint16_t eig, uint8_t eiw, int pos)
+{
+	unsigned long long mask_upper, mask_lower, dpa_upper_mask;
+	unsigned long long bits_upper, bits_lower;
+	unsigned long long hpa_offset, test_offset;
+	
 	/*
 	 * Translate DPA to HPA
 	 * Reverse the above decode logic to get back to an HPA
 	 * Insert the 'pos' to construct the HPA.
 	 */
 	if (eiw < 8) {
-		test_offset = (dpa_offset & MY_GENMASK_ULL(51, eig + 8)) << eiw;
-		test_offset |= pos << (eig + 8);
+		hpa_offset = (dpa_offset & MY_GENMASK_ULL(51, eig + 8)) << eiw;
+		hpa_offset |= pos << (eig + 8);
 	} else {
 		mask_upper = MY_GENMASK_ULL(51, eig + 8);
 		bits_upper = (dpa_offset & mask_upper) >> (eig + 8);
 		bits_upper = bits_upper * 3;
-		test_offset = ((bits_upper << (eiw - 8)) + pos) << (eig + 8);
+		hpa_offset = ((bits_upper << (eiw - 8)) + pos) << (eig + 8);
 	}
 
 	/* Lower bits don't change */
 	mask_lower = (1 << (eig + 8)) -1;
-	bits_lower = hpa_offset & mask_lower;
-	test_offset += bits_lower;
+	bits_lower = dpa_offset & mask_lower;
+	hpa_offset += bits_lower;
 
-	if (test_offset != hpa_offset)
-		exit(EXIT_FAILURE);
+	/* Now go back to the DPA offset for a sanity check */
+	test_offset = to_dpa(hpa_offset, eig, eiw);
 
-	exit(EXIT_SUCCESS);
+	if (test_offset != dpa_offset) {
+		printf("FAIL: test_offset=0x%llx dpa_offset=0x%llx\n",
+			test_offset, dpa_offset);
+		printf("FAIL: ");
+	} else {
+		printf("Pass: ");
+	}
+
+	printf("dpa_offset=0x%llx eig=%u eiw=%u pos=%d hpa_offset=0x%llx\n",
+	       dpa_offset, eig, eiw, pos, hpa_offset);
 }
+
+int eiw_to_ways(uint8_t eiw, unsigned int *ways)
+{
+        switch (eiw) {
+        case 0 ... 4:
+                *ways = 1 << eiw;
+                break;
+        case 8 ... 10:
+                *ways = 3 << (eiw - 8);
+                break;
+        default:
+                return -EINVAL;
+        }
+
+        return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned long long dpa_offset = strtoull(argv[1], NULL, 0);
+	int i, j, k, ways; 
+	
+	uint8_t eiws[] = {0, 1, 2, 3, 4, 8, 9, 10};
+	int nr_eiws = 8;
+	uint16_t eigs[] = {0, 1, 2, 3, 4, 5, 6};
+	int nr_eigs = 7;
+
+	if ((dpa_offset & 0x07) != 0) {
+		printf("Invalid dpa_offset - must be 64b aligned\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; i < nr_eigs; i++)
+		for (j = 1; j < nr_eiws; j++) {
+			eiw_to_ways(eiws[j], &ways);
+			for (k = 0; k < ways; k++) 
+				to_hpa(dpa_offset, eigs[i], eiws[j], k);
+		}
+}
+
+
